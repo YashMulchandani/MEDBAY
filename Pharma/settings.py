18eeda2ca0779ec02376735e60fcfa8f1bc158f1
Yash Mulchandani <yash.mulchandani123@gmail.com>
2021-05-15 13:23:31 +0530

    changes in navigation in pages and urls is edited and change in settings

--- Pharma/settings.py
+++ Pharma/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website',
+
+    'website.apps.WebsiteConfig',
 ]
 
 MIDDLEWARE = [
--- website/urls.py
+++ website/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name="index"),
-    path('home.html', views.home, name="home"),
-    path('contact.html', views.contact, name="contact"),
-    path('about.html', views.about, name="about"),
-    path('shop.html', views.shop, name="shop"),
-    path('shop_single.html/', views.shop_single, name="shop_single"),
-	path('cart.html', views.cart, name="cart"),
-	path('checkout.html', views.checkout, name="checkout"),
-    path('update_Item', views.update_Item, name="update_Item"),
-    path('process_order', views.processOrder, name="process_order"),
+    path('home/', views.home, name="home"),
+    path('contact/', views.contact, name="contact"),
+    path('about/', views.about, name="about"),
+    path('shop/', views.shop, name="shop"),
+    path('shop_single/', views.shop_single, name="shop_single"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+    path('update_Item/', views.updateItem, name="update_Item"),
+    path('process_order/', views.processOrder, name="process_order"),
 ]
